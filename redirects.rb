e0e18b6b95eb9d04b2e3ed88550ca757d28c1569
Samia Nneji <snneji@pivotal.io>
2019-11-19 13:44:18 +0000

    Fix redirect [#169783557]

--- redirects.rb
+++ redirects.rb
@@ -3,4 +3,4 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-r301 %r{/developer-console/(?![\d-]+)(.*)}, "/developer-console/0-5/$1"
+r301 %r{/developer-console/(?![\d-]+)(.*)}, "/developer-console/0-1/$1"
