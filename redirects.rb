58cb3eeb582ee4ea1ffbfeaadcf11f863e767ebe
jncd <jday@pivotal.io>
2019-12-17 14:20:32 -0800

    use a temp redirect[#170164916]

--- redirects.rb
+++ redirects.rb
@@ -5,5 +5,5 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 
 # r301 %r{/developer-console/(?![\d-]+)(.*)}, "/developer-console/0-1/$1"
 
-r301 %r{/developer-console/(?![\d-]+)(.*)}, "/developer-console/0-alpha/$1"
+r302 %r{/developer-console/(?![\d-]+)(.*)}, "/developer-console/0-alpha/$1"
 r301 %r{/developer-console/0-1/(.*)}, "/developer-console/0-alpha/$1"
