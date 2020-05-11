c5d2a6db952e14e10e1eec76e4ce3538c088968d
Samia Nneji <33425661+snneji@users.noreply.github.com>
2020-05-11 15:08:37 +0100

    Remove unversioned redirect [#172510373]

--- redirects.rb
+++ redirects.rb
@@ -3,9 +3,6 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# r301 %r{/developer-console/(?![\d-]+)(.*)}, "/developer-console/0-1/$1"
-
-r302 %r{/developer-console/(?![\d-]+)(.*)}, "/developer-console/0-alpha/$1"
 r301 %r{/developer-console/0-1/(.*)}, "/developer-console/0-alpha/$1"
 r301 %r{/developer-console/0-2/(.*)}, "/developer-console/0-alpha/$1"
 r301 %r{/developer-console/0-3/(.*)}, "/developer-console/0-alpha/$1"
