723893b99e92c35a71b00b458f4a387397c69241
Samia Nneji <snneji@pivotal.io>
2020-06-08 17:26:27 +0100

    Update redirects [#173222807]

--- redirects.rb
+++ redirects.rb
@@ -3,5 +3,6 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# All version numbered docs go to alpha
+# All versions go to 0-alpha
 r301 %r{/developer-console/([\d-]+)/(.*)}, "/developer-console/0-alpha/$2"
+r301 %r{/developer-console/alpha/(.*)}, "/developer-console/0-alpha/$1"
