bd7e7e5aed85be55a4c13e0a67e34101be9ae872
Samia Nneji <33425661+snneji@users.noreply.github.com>
2020-06-11 14:55:08 +0100

    Will this fix it? [#173265810]

--- redirects.rb
+++ redirects.rb
@@ -2,3 +2,5 @@
 r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
+
+r302 %r{/developer-console/(?![\d-]+)(.*)}, "/developer-console/0-alpha/$1"
